a3394e8f97ad2c7ae7b54692ab9dc4d92ffd81cb
brokenCart <yathartgpta@gmail.com>
2026-04-16 13:12:21 +0530

    added LED fade in and fade out program

---
+++ ledFadeInFadeOut.ino
@@ -0,0 +1,21 @@
+#define LED_PIN 10
+
+void setup()
+{
+    pinMode(LED_PIN, OUTPUT);
+}
+
+void loop()
+{
+    for (int i = 0; i <= 255; i++)
+    {
+        analogWrite(LED_PIN, i);
+        delay(50);
+    }
+
+    for (int i = 255; i >= 0; i--)
+    {
+        analogWrite(LED_PIN, i);
+        delay(50);
+    }
+}
